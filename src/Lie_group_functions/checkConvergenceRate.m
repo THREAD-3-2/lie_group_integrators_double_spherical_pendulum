0281f70a2e83f1f51c6480aad235309e26e20a9f
Ergys Çokaj <ergys.cokaj@ntnu.no>
2022-08-23 09:46:36 +0200

    change of names

--- src/Lie_group_functions/checkConvergenceRate.m
+++ src/Lie_group_functions/checkConvergenceRate.m
@@ -61,7 +61,7 @@ function [] = checkConvergenceRate(f,action,vecField,z0,L,m)
             z1 = LieEulerSE3N(vecField, action, z1, dt);
             z2 = LieEulerHeunSE3N(vecField, action, z2, dt);
             z3 = FreeRK4SE3N(f, action, dt, z3);
-            z4 = LieRK4_SE3N(vecField, action, z4, dt);
+            z4 = RKMK4(vecField, action, z4, dt);
             z5 = TwoCommRK4SE3N(f, action, dt, z5);
             z6 = LieRK3_SE3N(vecField, action, z6, dt);
         end
