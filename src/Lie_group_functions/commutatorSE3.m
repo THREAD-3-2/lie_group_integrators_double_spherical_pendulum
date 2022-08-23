8e87d7b76113770f2c609a47eb20b7f6d8d75175
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-23 10:28:02 +0200

    Update commutatorSE3.m

--- src/Lie_group_functions/commutatorSE3.m
+++ src/Lie_group_functions/commutatorSE3.m
@@ -1,17 +1,19 @@
-function res = commutatorSE3(u,v)
-    
-    %This method computes the commutator [u,v] = [(u1,u2),(v1,v2)] = 
-    %(u1 x v1, u1 x v2 - v1 x u2).
+function res = commutatorSE3(u, v)
+% Computes the commutator
+%
+% :param u: element in the Lie algebra se(3), represented as a 6x1 vector  
+% :param v: element in the Lie algebra se(3), represented as a 6x1 vector  
+%
+% :returns: commutator [u,v] = [(u1,u2),(v1,v2)] = (u1 x v1, u1 x v2 - v1 x u2)
 
-    u1 = u(1:3);
-    u2 = u(4:end);
+    u1 = u(1 : 3);
+    u2 = u(4 : end);
     
-    v1 = v(1:3);
-    v2 = v(4:end);
+    v1 = v(1 : 3);
+    v2 = v(4 : end);
     
-    res = zeros(6,1);
-    res(1:3) = hat(u1)*v1;
-    res(4:end) = hat(u1)*v2 - hat(v1)*u2;
-    
-
+    res = zeros(6, 1);
+    res(1 : 3) = hat(u1) * v1;
+    res(4 : end) = hat(u1) * v2 - hat(v1) * u2;
+   
 end
