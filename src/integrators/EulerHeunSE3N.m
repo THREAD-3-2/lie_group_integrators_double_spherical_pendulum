f8795435f5d91122c57cde4e9d3569e448ba936d
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-23 09:32:18 +0200

    Update EulerHeunSE3N.m

--- src/integrators/EulerHeunSE3N.m
+++ src/integrators/EulerHeunSE3N.m
@@ -1,10 +1,16 @@
-function sol = EulerHeunSE3N(vecField,action,p,h)
+function sol = LieEulerHeunSE3N(vecField, action, p, h)
+% Runge-Kutta-Munthe-Kaas time integrator order 2, based on Heun scheme
+%
+% :param vecField: right hand side of the ODE
+% :param action: Lie group action
+% :param p: solution at time t_n
+% :param h: time step size
+%
+% :returns: solution at time t_(n+1)
 
-    %Computes one time step update with Lie Euler Heun's method
-
-    k0 = zeros(length(p),1);
-    k1 = vecField(k0,p);
-    k2 = vecField(h*k1, p);
-    sol = action(exponentialSE3N(h/2*(k1+k2)),p);
+    k0 = zeros(length(p), 1);
+    k1 = vecField(k0, p);
+    k2 = vecField(h * k1, p);
+    sol = action(exponentialSE3N(h/2 * (k1 + k2)), p);
 
 end
