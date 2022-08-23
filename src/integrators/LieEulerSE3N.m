3c777c8adc12109edc121941e9ca9101015dd344
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-23 09:35:04 +0200

    Update LieEulerSE3N.m

--- src/integrators/LieEulerSE3N.m
+++ src/integrators/LieEulerSE3N.m
@@ -1,10 +1,16 @@
-function sol = LieEulerSE3N(vecField,action,p,h)
+function sol = LieEulerSE3N(vecField, action, p, h)
+% Lie-Euler time integrator (Runge-Kutta-Munthe-Kaas method of order 1)
+%
+% :param vecField: right hand side of the ODE
+% :param action: Lie group action
+% :param p: solution at time t_n
+% :param h: time step size
+%
+% :returns: solution at time t_(n+1) order 1
 
-    %Computes one time step update with Lie Euler 
+    k0 = zeros(length(p), 1);
+    k1 = vecField(k0, p);
 
-    k0 = zeros(length(p),1);
-    k1 = vecField(k0,p);
-
-    sol = action(exponentialSE3N(h*k1),p);   
+    sol = action(exponentialSE3N(h * k1), p);   
     
 end
