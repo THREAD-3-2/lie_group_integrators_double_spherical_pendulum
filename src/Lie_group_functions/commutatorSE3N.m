71cba7aad9102143fcba125496c4630c063c286b
Ergys Çokaj <88881376+ergyscokaj@users.noreply.github.com>
2022-08-23 10:29:41 +0200

    Update commutatorSE3N.m

--- src/Lie_group_functions/commutatorSE3N.m
+++ src/Lie_group_functions/commutatorSE3N.m
@@ -1,12 +1,15 @@
-function res = commutatorSE3N(u,v)
+function res = commutatorSE3N(u, v)
+% Computes the commutator
+%
+% :param u: element of se(3)^N 
+% :param v: element of se(3)^N 
+%
+% :returns: commutator [u, v]
     
-    N =  length(u)/6; %number of connected pendulums
-    res = zeros(length(u),1);
+    N =  length(u)/6;    %number of connected pendulums
+    res = zeros(length(u), 1);
     
-    for i = 1:N
-        res(6*i-5:6*i) = commutatorSE3(u(6*i-5:6*i),v(6*i-5:6*i)); %exploit the 
-        %direct product structure of the Lie group SE(3)^N
+    for i = 1 : N
+        res(6 * i - 5 : 6 * i) = commutatorSE3(u(6 * i - 5 : 6 * i), v(6 * i - 5 : 6 * i)); %exploit the direct product structure of the Lie group SE(3)^N
     end
-    
-
 end
